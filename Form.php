9991bc31ebc150bab47025f69d149b8142893332
Adegbola-GNA <114452299+Adegbola-GNA@users.noreply.github.com>
2022-10-15 02:23:04 +0100

    Create Form.php

---
+++ Form.php
@@ -0,0 +1,23 @@
+<form method= "POST" action="userdata.php">
+
+<label for="name">
+Name: <input type="text" name="name">
+</label> <br>
+
+<label for="email">
+Email: <input type="email" name="email">
+</label> <br>
+
+<label for="birthday">
+Date of Birth: <input type="date" name="birthday">
+</label> <br>
+
+<label for="Gender">
+Gender: <input type="text" name="gender">
+</label> <br>
+
+<label for="Country">
+Country: <input type="text" name="country">
+</label> <br>
+
+<button type="submit">Submit</button>
